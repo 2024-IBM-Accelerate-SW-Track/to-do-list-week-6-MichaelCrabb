408d224168ad6776fdba6752c3f47695f50e78d8
Michael Crabb <Michael.M.Crabb-1@ou.edu>
2024-07-15 20:37:02 -0500

    Requirements

--- src/component/todos.js
+++ src/component/todos.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../component/todos.css";
-import { Card, Grid, ListItemButton, ListItemText, Checkbox} from "@mui/material";
+import {Card,CardContent,Grid,ListItemButton,ListItemText,Checkbox,} from "@mui/material";
 
 // 1. This component formats and returns the list of todos.
 // 2. Treat the question mark like an if statement.
@@ -10,36 +10,46 @@ import { Card, Grid, ListItemButton, ListItemText, Checkbox} from "@mui/material
 // 4. Think of lines 14-23 as a loop. For each todo in the todo list, we want to give the list item
 // a key, and it's own card shown in the UI
 const Todos = ({ todos, deleteTodo }) => {
-  const todoList = todos.length ? (
-    todos.map((todo) => {
-      let color = "#ffffffff"
-      if (new Date(todo.duedate) < new Date()){
-        color = 'red'
-      }
-      console.log(color)
-      return (
-        <Grid key={todo.id}>
-          <Card data-testid={todo.content} style={{marginTop:10, background: color}}>
-            {/* Remember, we set the local state of this todo item when the user submits the form in 
-            AddTodo.js. All we need to do is return the todo list item {todo.content} as well as its 
-            current date/time {todo.date}. Also, the item's id is utilized in order to correctly delete an item from the Todo list*/}
-            <ListItemButton component="a" href="#simple-list">
-              <Checkbox style={{paddingLeft:0}} color="primary" onClick={() => deleteTodo(todo.id)}/>
-              <ListItemText primary={todo.content} secondary={todo.duedate}/>
-            </ListItemButton>
-          </Card>
-        </Grid>
-      );
-    })
-  ) : (
-    <p>You have no todo's left </p>
-  );
-  // Lastly, return the todoList constant that we created above to show all of the items on the screen.
-  return (
-    <div className="todoCollection" style={{ padding: "10px" }}>
-      {todoList}
-    </div>
-  );
-};
+    const todoList = todos.length ? (
+      todos.map((todo) => {
+        let color = "";
+        let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
+           "October", "November", "December"]
+        let due_date = new Date(todo.due_date);
+        let due_week_day = days[due_date.getDay() - 1];
+        let due_month = months[due_date.getMonth() - 1];
+        let due_month_day = due_date.getDate();
+        for (let i = 0; i < 31; ++i) {
 
+        }
+        if (todo.due_date > todo.date) {
+          color = "red";
+        }
+        else {
+          color = "white";
+        }
+        return (
+          <Grid key={todo.id} className="grid">
+            <Card className="card" data-testid={todo.content} style = {{ backgroundColor: color }}>
+              <ListItemButton component="a" href="#simple-list">
+                <Checkbox style={{ paddingLeft: 0 }} color="primary" onClick={ () => { deleteTodo(todo.id) }}/>
+                <ListItemText primary={todo.content} secondary={"Due: " + due_week_day + ", " + due_month
+                  + " " + due_month_day}/>
+              </ListItemButton>
+            </Card>
+          </Grid>
+        );
+      })
+    ) : (
+      <p>You have no more tasks left. Time to relax.</p>
+    );
+    // Lastly, return the todoList constant that we created above to show all of the items on the screen.
+    return (
+      <div className="todoCollection" style={{ padding: "10px" }}>
+        {todoList}
+      </div>
+    );
+};
+  
 export default Todos;
